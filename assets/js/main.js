db3ffa915be1a7cb72bb748f8d4602adcbc15a1c
rodrigo <rodrigocesar068@gmail.com>
2023-06-13 23:39:20 +0044

    alteraçao portifolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,17 @@ function updateLanguages(profileData) {
     languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
 }
 
-
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.portfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+            <li>
+                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+                <a href="${project.url}" target="_blank">${project.url}</a>
+            </li>
+        `
+    }).join('')
+}
 
 (async () => {
 
