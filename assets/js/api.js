948585860c442c3409c2c5a3b77b9ac8dfb744a7
rodrigo <rodrigocesar068@gmail.com>
2023-06-14 00:59:59 +0044

    Adicinando img

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,8 @@
-async function fetchProfileDate () {
-    
-    const url = 'https://raw.githubusercontent.com/rodrigocsar/Portifolio/master/data/profile.json'
-    const fetching = await fetch(url)
-    return await fetching.json()
+
+
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/rodrigocsar/Portifolio/master/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,25 @@
 
-
-function updateProfileInfo(profileDate) {
+function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
-    photo.src = profileDate.photo
-    photo.alt = profileDate.name
+    photo.src = profileData.photo
+    photo.alt = profileData.name
 
     const name = document.getElementById('profile.name')
-    name.innerText = profileDate.name
+    name.innerText = profileData.name
 
     const job = document.getElementById('profile.job')
-    job.innerText = profileDate.job
+    job.innerText = profileData.job
 
     const location = document.getElementById('profile.location')
-    location.innerText = profileDate.location
+    location.innerText = profileData.location
 
     const phone = document.getElementById('profile.phone')
-    phone.innerText = profileDate.phone
-    phone.href = `tel:${profileDate.phone}`
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
 
     const email = document.getElementById('profile.email')
-    email.innerText = profileDate.email
-    email.href = `mailto:${profileDate.email}`
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
 }
 
 function updateSoftSkills(profileData) {
@@ -38,6 +37,11 @@ function updateLanguages(profileData) {
     languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
 }
 
+function updateFormation(profileData){
+    const education = document.getElementById('profile.formationAcad')
+    education.innerHTML = profileData.education.formation.map(form => `<li><img src="${form.logo}" alt="${form.name}" title="${form.name}"></li>`).join('')
+}
+
 function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio')
     portfolio.innerHTML = profileData.portfolio.map(project => {
@@ -50,14 +54,26 @@ function updatePortfolio(profileData) {
     }).join('')
 }
 
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return `
+            <li>
+                <h3 class="title">${experience.name}</h3>
+                <p class="period">${experience.period}</p>
+                <p>${experience.description}</p>
+            </li>
+        `
+    }).join('')
+}
+
 (async () => {
-
-    const profileDate = await fetchProfileDate()
-    updateProfileInfo(profileDate)
-    updateSoftSkills(profileDate)
-    updateHardSkills(profileDate)
-    updateLanguages(profileDate) 
-    console.log(profileDate)
-
-
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updatePortfolio(profileData)
+    updateProfessionalExperience(profileData)
+    updateFormation(profileData)
 })()
