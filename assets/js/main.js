ab8778300a65a3f830305f51aaabd58f281783b5
rodrigo <rodrigocesar068@gmail.com>
2023-06-14 03:13:37 +0044

    nda

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ function updateProfileInfo(profileData) {
     const photo = document.getElementById('profile.photo')
     photo.src = profileData.photo
     photo.alt = profileData.name
+     
 
     const name = document.getElementById('profile.name')
     name.innerText = profileData.name
