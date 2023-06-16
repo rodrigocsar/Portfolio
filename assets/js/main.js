b0832703870e605d894ae68be8e00fa8832d4d33
rodrigo <rodrigocesar068@gmail.com>
2023-06-16 16:26:08 +0044

    corrigindo erros

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,7 +62,7 @@ function updatePortfolio(profileData) {
         return `
             <li>
                 <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
-                <a href="${project.url}" target="_blank">${project.description}</a>
+                <a href="${project.url}" target="_blank">${project.url}</a>
             </li>
         `
     }).join('')
